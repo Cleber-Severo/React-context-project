dd3cea01da61067a0d614c238ca438c519bc8941
Cléber Severo <clebersevero1998@gmail.com>
2023-03-20 21:30:14 -0300

    jusing useContext

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -19,48 +19,41 @@ function Login() {
   const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
   return (
     <Container>
-      <UsuarioContext.Consumer>
-        {
-          ({nome, setNome, saldo, setSaldo}) => (
-            <>
-              <Titulo>
-                Insira o seu nome
-              </Titulo>
-              <InputContainer>
-                <InputLabel>
-                  Nome
-                </InputLabel>
-                <Input
-                  value={nome}
-                  onChange={event => setNome(event.target.value)}
-                  type="text"
-                />
-              </InputContainer>
-              <InputContainer>
-                <InputLabel>
-                  Saldo
-                </InputLabel>
-                <Input
-                  type="number"
-                  value={saldo}
-                  onChange={event => setSaldo(event.target.value)}
-                  startAdornment={
-                    <InputAdornment position="start">
-                      R$
-                    </InputAdornment>
-                  }
-                />
-              </InputContainer>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => { history.push('/feira') }}
-              >
-                Avançar
-              </Button>
-            </>
-          )}
-      </UsuarioContext.Consumer>
+      <Titulo>
+        Insira o seu nome
+      </Titulo>
+      <InputContainer>
+        <InputLabel>
+          Nome
+        </InputLabel>
+        <Input
+          value={nome}
+          onChange={event => setNome(event.target.value)}
+          type="text"
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel>
+          Saldo
+        </InputLabel>
+        <Input
+          type="number"
+          value={saldo}
+          onChange={event => setSaldo(event.target.value)}
+          startAdornment={
+            <InputAdornment position="start">
+              R$
+            </InputAdornment>
+          }
+        />
+      </InputContainer>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => { history.push('/feira') }}
+      >
+        Avançar
+      </Button>
     </Container>
   )
 };
