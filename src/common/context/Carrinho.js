61cf0b948b9ae85a0c190b52aef9527d387962a4
Cléber Severo <clebersevero1998@gmail.com>
2023-03-22 20:50:07 -0300

    custom Hooks to isolate the responsability of handling the logic to add a product and addint it to the product component

--- src/common/context/Carrinho.js
+++ src/common/context/Carrinho.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 
 export const CarrinhoContext = createContext();
 CarrinhoContext.displayName = 'Carrinho';
@@ -6,8 +6,34 @@ CarrinhoContext.displayName = 'Carrinho';
 export const CarrinhoProvider = ({ children }) => {
     const [carrinho, setCarrinho] = useState([]);
     return (
-        <CarrinhoContext.Provider value={{carrinho, setCarrinho}} >
+        <CarrinhoContext.Provider value={{ carrinho, setCarrinho }} >
             {children}
         </CarrinhoContext.Provider>
     )
+}
+
+export const useCarrinhoContext = () => {
+    const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+    function adicionarProduto(novoProduto) {
+        const temOProduto = carrinho.some(itemDoCarrinho => itemDoCarrinho.id === novoProduto.id);
+        novoProduto.quantidade = 1;
+        if (!temOProduto) {
+
+            return setCarrinho(carrinhoAnterior =>
+                [...carrinhoAnterior, novoProduto]);
+        }
+
+        setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemDoCarrinho => {
+            if (itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
+            return itemDoCarrinho
+        }))
+
+    }
+
+    return {
+        carrinho,
+        setCarrinho,
+        adicionarProduto
+    }
 }
