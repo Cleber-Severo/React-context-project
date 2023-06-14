398159e53fa9fd14918d3d3abbe8bc5e2b85e872
Cléber Severo <clebersevero1998@gmail.com>
2023-06-14 12:00:21 -0300

    selecting payment method, select from material UI

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,4 +1,4 @@
-import { Button, Snackbar, InputLabel } from '@material-ui/core';
+import { Button, Snackbar, InputLabel, Select, MenuItem } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
 import { useCarrinhoContext } from 'common/context/Carrinho';
 import Produto from 'components/Produto';
@@ -8,16 +8,16 @@ import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { useContext } from 'react';
 import { PagamentoContext } from 'common/context/pagamento';
 
+
 function Carrinho() {
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const {carrinho } = useCarrinhoContext();
-  const { formaPagamento } = useContext(PagamentoContext)
+  const { tiposPagamento, formaPagamento, setFormaPagamento } = useContext(PagamentoContext)
   const history = useHistory();
 
   return (
     <Container>
       <Voltar onClick={() => history.goBack()} />
-      {formaPagamento.nome}
       <h2>
         Carrinho
       </h2>
@@ -29,6 +29,16 @@ function Carrinho() {
       ))}
       <PagamentoContainer>
         <InputLabel> Forma de Pagamento </InputLabel>
+        <Select 
+          value={formaPagamento.id}
+          onChange={(event) => setFormaPagamento(event.target.value)} 
+        >
+          {tiposPagamento.map( pagamento => (
+            <MenuItem value={pagamento.id} key={pagamento.id} >  
+              {pagamento.nome}
+            </MenuItem>
+          ) )}
+        </Select>
       </PagamentoContainer>
       <TotalContainer>
           <div>
